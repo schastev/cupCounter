433fceaace0ecb09e9789a0df0f974afdbdeda03
schastev <62057364+schastev@users.noreply.github.com>
2022-06-18 01:08:20 +0500

    test: Improve existing tests
    The loading checks two views and the searching one accounts for different scenarios based on the number of entries found.

---
+++ app/src/androidTest/java/com/tasty/count/CustomMatchers.java
@@ -0,0 +1,64 @@
+package com.tasty.count;
+
+import static androidx.test.espresso.core.internal.deps.guava.base.Preconditions.checkNotNull;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.matcher.BoundedMatcher;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public final class CustomMatchers {
+
+    public static Matcher<View> hasTextInputLayoutErrorText(final String expectedErrorText) {
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof TextInputLayout)) {
+                    return false;
+                }
+
+                CharSequence error = ((TextInputLayout) view).getError();
+
+                if (error == null) {
+                    return false;
+                }
+
+                String hint = error.toString();
+
+                return expectedErrorText.equals(hint);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+            }
+        };
+    }
+    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
+        checkNotNull(itemMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has item at position " + position + ": ");
+                itemMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    // has no item on such position
+                    return false;
+                }
+                return itemMatcher.matches(viewHolder.itemView);
+            }
+        };
+    }
+}
--- app/src/androidTest/java/com/tasty/count/activity/MainActivityTest.java
+++ app/src/androidTest/java/com/tasty/count/activity/MainActivityTest.java
@@ -1,9 +1,7 @@
 package com.tasty.count.activity;
 
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -11,17 +9,21 @@ import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.tasty.count.CustomMatchers.hasTextInputLayoutErrorText;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.instanceOf;
 
 import android.content.Context;
+import android.content.res.Resources;
 
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.tasty.count.CustomMatchers;
 import com.tasty.count.R;
 import com.tasty.count.database.AppDatabase;
 import com.tasty.count.database.Customer;
@@ -34,26 +36,35 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
     private CustomerDAO customerDAO;
+    private Resources res;
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Before
-    public void databaseSetup() {
+    public void setup() {
         Context context = ApplicationProvider.getApplicationContext();
         AppDatabase db = DBClient.getInstance(context).getAppDatabase();
         customerDAO = db.customerDao();
+        res = InstrumentationRegistry.getInstrumentation().getTargetContext().getResources();
+        for (int i = 0; i < 10; i++) {
+            Customer customer = new Customer();
+            int id = (int) customerDAO.insert(customer);
+            customer.setId(id);
+        }
     }
 
     @Test
     public void mainActivityLoads() {
-        onView(withId(R.id.my_toolbar))
+        onView(allOf(withId(R.id.my_toolbar),
+                withId(R.id.main_field_search)))
                 .check(matches(isDisplayed()));
     }
 
@@ -62,15 +73,25 @@ public class MainActivityTest {
         int random = (int) ((Math.random() * 9));
         String search = String.valueOf(random);
         onView(instanceOf(TextInputEditText.class)).perform(typeText(search)).perform(pressImeActionButton());
-        List<Customer> actual = customerDAO.findByShortNumber("%" + search);
+        List<Customer> actual = customerDAO.findByShortNumber("%" + search)
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
         int resultSize = actual.size();
-        for (int i = 0; i < resultSize; i++) {
-            onData(withId(R.id.main_list_results))
-                    .atPosition(i)
-                    .check(matches(hasDescendant(withText(actual.get(i).getPhoneNumber()))));
-
-            onView(withId(R.id.main_list_results))
-                    .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        if (resultSize == 0) {
+            onView(withId(R.id.main_field_search))
+                    .check(matches(hasTextInputLayoutErrorText(res.getString(R.string.main_no_result))));
+        } else if (resultSize == 1) {
+            Customer customer = actual.get(0);
+            onView(withId(R.id.info_field_container_phone)).check(matches(withText(customer.getPhoneNumber())));
+        } else {
+            for (int i = 0; i < resultSize; i++) {
+                Customer customer = actual.get(i);
+                onView(withId(R.id.main_list_results))
+                        .check(matches(CustomMatchers.atPosition(i, hasDescendant(withText(customer.getName())))));
+                onView(withId(R.id.main_list_results))
+                        .check(matches(CustomMatchers.atPosition(i, hasDescendant(withText(customer.getPhoneNumber())))));
+            }
         }
     }
 }
