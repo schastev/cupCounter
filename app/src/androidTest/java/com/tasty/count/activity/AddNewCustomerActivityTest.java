7036bc5194f2478fb8773537e02f93763a25cde6
schastev <62057364+schastev@users.noreply.github.com>
2022-06-18 23:15:36 +0500

    test: Make AddNewCustomerActivityTest launch the tested activity immediately

--- app/src/androidTest/java/com/tasty/count/activity/AddNewCustomerActivityTest.java
+++ app/src/androidTest/java/com/tasty/count/activity/AddNewCustomerActivityTest.java
@@ -3,6 +3,7 @@ package com.tasty.count.activity;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -18,7 +19,6 @@ import com.tasty.count.R;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.hamcrest.Matcher;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,17 +28,12 @@ import org.junit.runner.RunWith;
 public class AddNewCustomerActivityTest {
 
     @Rule
-    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
-            new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<AddNewCustomerActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(AddNewCustomerActivity.class);
     private Matcher<View> phoneInput = withId(R.id.new_input_phone);
     private Matcher<View> nameInput = withId(R.id.new_input_name);
     private Matcher<View> cupSwitch = withId(R.id.new_switch_add_cup);
     private Matcher<View> addButton = withId(R.id.new_button_add);
-
-    @Before
-    public void openActivity() {
-        onView(withId(R.id.toolbar_button_add)).perform(click());
-    }
 
     @Test
     public void activityOpensViaToolbarButtonClick() {
@@ -49,8 +44,8 @@ public class AddNewCustomerActivityTest {
 
     @Test
     public void happyPath() {
-        onView(nameInput).perform(typeText(RandomStringUtils.randomAlphabetic(5)));
-        onView(phoneInput).perform(typeText(RandomStringUtils.randomNumeric(10)));
+        onView(phoneInput).perform(typeText(RandomStringUtils.randomNumeric(10)), closeSoftKeyboard());
+        onView(nameInput).perform(typeText(RandomStringUtils.randomAlphabetic(5)), closeSoftKeyboard());
         onView(addButton).perform(click());
         onView(withId(R.id.main_field_search)).check(matches(isDisplayed()));
     }
