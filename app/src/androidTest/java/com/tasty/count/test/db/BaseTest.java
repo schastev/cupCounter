687d8e8f591d51f20e7f21189ada11fe00e2f3cf
schastev <62057364+schastev@users.noreply.github.com>
2022-06-25 16:28:05 +0500

    fix: Remove runner from base test class

--- app/src/androidTest/java/com/tasty/count/test/db/BaseTest.java
+++ app/src/androidTest/java/com/tasty/count/test/db/BaseTest.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.tasty.count.database.AppDatabase;
 import com.tasty.count.database.Customer;
@@ -12,12 +11,11 @@ import com.tasty.count.database.CustomerDAO;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RunWith(AndroidJUnit4.class)
+
 public class BaseTest {
     public CustomerDAO customerDAO;
     private AppDatabase db;
